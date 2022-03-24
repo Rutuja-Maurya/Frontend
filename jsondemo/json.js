a5823de6c2e54edf6bd7ef8b4747ee81bc52222e
Rutuja-Maurya <rutujamaurya0707@gmail.com>
2022-03-24 18:34:06 +0530

    task done

---
+++ jsondemo/json.js
@@ -0,0 +1,8 @@
+function Show() {
+    var getfname = document.getElementById("firstName").value;
+    var getlname = document.getElementById("LastName").value;
+    var getphone = document.getElementById("phn").value;
+    console.log(getphone);
+    console.log(getfname);
+    console.log(getlname);
+}
